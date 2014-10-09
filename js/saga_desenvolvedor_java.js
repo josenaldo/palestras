ce2b3523b8ddf4d199fa875852e4fa2a3bc00d00
josenaldo <josenaldo@gmail.com>
2014-10-09 18:09:32 -0300

    Mouse

--- js/saga_desenvolvedor_java.js
+++ js/saga_desenvolvedor_java.js
@@ -21,3 +21,12 @@ Reveal.initialize({
 		{ src: 'plugin/notes/notes.js', async: true, condition: function() { return !!document.body.classList; } }
 	]
 });
+
+window.addEventListener("mousedown", handleClick, false);
+window.addEventListener("contextmenu", function(e) { e.preventDefault(); }, false);
+
+function handleClick(e) {
+	e.preventDefault();
+	if(e.button === 0) Reveal.next();
+	if(e.button === 2) Reveal.prev();
+}
